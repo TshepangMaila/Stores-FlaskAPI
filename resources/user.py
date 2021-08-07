b7100f5220b8842d9fa4fcfade1278e9ac7c4460
TshepangMaila <tshepang.maila@sambeconsulting.com>
2021-08-07 14:55:58 +0200

    First Commit, added REST API

---
+++ resources/user.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+
+	parser = reqparse.RequestParser()
+
+	parser.add_argument('username',
+		type=str,
+		required=True,
+		help="This field is required"
+		)
+
+	parser.add_argument('password',
+		type=str,
+		required=True,
+		help="This field is required"
+		)
+
+	def post(self):
+
+		data = UserRegister.parser.parse_args()
+
+		if User.find_by_username(data['username']):
+			return {"message": "user with that username already exists"}
+
+		user = UserModel(**data)
+		user.save_to_db()
+
+		return {"message": "user created"}
